aab86560dc5dcfdbe596b399327b3619235aa66e
HooMaN <eng.homan@gmail.com>
2025-07-30 22:52:35 +0330

    feat: :sparkles: I developed the user profile page with a dedicated layout and sidebar
    I implemented a dedicated layout for the user profile, including a sidebar for navigation. The profile page now displays user information such as name and join date, formatted using a new `toLocalDateString` utility.

---
+++ src/app/(profile)/profile/SideBar.jsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+
+export default function SideBar() {
+  return (
+    <div>
+      <ul className="flex flex-col space-y-8">
+        <li>
+          <Link href="/">صفحه اصلی</Link>
+        </li>
+        <li>
+          <Link href="/me">اطلاعات کاربری</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -1,3 +1,23 @@
+"use client";
+import { useGetUser } from "@/hooks/useAuth";
+import { toLocalDateString } from "@/utils/toLocalDate";
+import Loading from "@/common/Loading";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const { data, isLoading } = useGetUser();
+  const { user } = data || {};
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <div className="py-4">
+      <h1 className="mb-4 text-xl">
+        سلام ! <span className="font-bold">{user.name}</span> خوش آمدی!
+      </h1>
+      <p>
+        <span>تاریخ پیوستن:</span>
+        <span> {toLocalDateString(user.createdAt)} </span>
+      </p>
+    </div>
+  );
 }
