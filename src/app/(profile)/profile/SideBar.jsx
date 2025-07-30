89de02b35b02c1e0cbeab3fed89673cedff86e3f
HooMaN <eng.homan@gmail.com>
2025-07-31 00:52:48 +0330

    feat: :door: I added the logout functionality to the profile sidebar
    I implemented a logout button in the profile sidebar that clears the user session and redirects to the homepage. I also added a "Dashboard" link to the profile section.

--- src/app/(profile)/profile/SideBar.jsx
+++ src/app/(profile)/profile/SideBar.jsx
@@ -1,7 +1,16 @@
 "use client";
+import { logout } from "@/services/authServices";
 import Link from "next/link";
 
 export default function SideBar() {
+  const logoutHandler = async () => {
+    await logout();
+    // localStorage.removeItem("userInfo");
+    // localStorage.removeItem("cartItems");
+    // localStorage.removeItem("token");
+    document.location.href = "/";
+  };
+
   return (
     <div>
       <ul className="flex flex-col space-y-8">
@@ -9,7 +18,13 @@ export default function SideBar() {
           <Link href="/">صفحه اصلی</Link>
         </li>
         <li>
+          <Link href="/profile">داشبورد</Link>
+        </li>
+        <li>
           <Link href="/profile/me">اطلاعات کاربری</Link>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>خروج از حساب کاربری</button>
         </li>
       </ul>
     </div>
