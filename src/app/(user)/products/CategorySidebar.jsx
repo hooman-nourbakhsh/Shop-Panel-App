01c8581f92989af6848b91dba72e786b399b64bf
HooMaN <eng.homan@gmail.com>
2025-07-31 13:52:52 +0330

    feat: :arrow_up_down: I added product sorting functionality
    I implemented product sorting options (latest, earliest, popular) on the product listing page. This includes a new `ProductsSort` component and a reusable `RadioInput` component for selection.

--- src/app/(user)/products/CategorySidebar.jsx
+++ src/app/(user)/products/CategorySidebar.jsx
@@ -1,10 +1,11 @@
-"use client";
 import ProductsFilter from "./ProductsFilter";
+import ProductsSort from "./ProductsSort";
 
 function CategorySidebar({ categories }) {
   return (
     <div className="col-span-1">
       <ProductsFilter categories={categories} />
+      <ProductsSort />
     </div>
   );
 }
